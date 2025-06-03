8bdabc210cfabf211a8bc35e80e960b132a54545
Kufieta-Jakub <kufietajakub@gmail.com>
2025-06-03 13:24:34 +0200

    Creating Order and Product models

---
+++ KomputerowoE-commerce/Models/Order.cs
@@ -0,0 +1,10 @@
+﻿namespace KomputerowoE_commerce.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public DateTime OrderDate { get; set; }
+        public int[] Product_ID { get; set; }
+        public string CustomerName { get; set; }
+    }
+}
