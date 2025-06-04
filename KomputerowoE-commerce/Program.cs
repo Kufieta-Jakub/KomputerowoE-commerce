f85f62b364ba39b9dadf67a04aaeee52c3bbc80e
Kufieta-Jakub <kufietajakub@gmail.com>
2025-06-04 23:27:43 +0200

    change

--- KomputerowoE-commerce/Program.cs
+++ KomputerowoE-commerce/Program.cs
@@ -36,4 +36,11 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    dbContext.Database.Migrate();
+}
+
+
 app.Run();
